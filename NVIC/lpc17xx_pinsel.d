653b373be27983cfc8b3ff6648322bec2b6861bd
mojtaba rostami <mrbilandi@gamil.com>
2020-03-15 16:30:15 +0330

    NVIC using lpc1768

---
+++ NVIC/lpc17xx_pinsel.d
@@ -0,0 +1,9 @@
+lpc17xx_pinsel.o: ..\..\..\..\CMSIS\lpc175x_6x_cmsis_driver_library\Drivers\source\lpc17xx_pinsel.c
+lpc17xx_pinsel.o: F:\Keil\ARM\Inc\NXP\LPC17xx\lpc17xx_pinsel.h
+lpc17xx_pinsel.o: F:\Keil\ARM\Inc\NXP\LPC17xx\lpc17xx.h
+lpc17xx_pinsel.o: F:\Keil\ARM\CMSIS\Include\core_cm3.h
+lpc17xx_pinsel.o: F:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+lpc17xx_pinsel.o: F:\Keil\ARM\CMSIS\Include\core_cmInstr.h
+lpc17xx_pinsel.o: F:\Keil\ARM\CMSIS\Include\core_cmFunc.h
+lpc17xx_pinsel.o: F:\Keil\ARM\Inc\NXP\LPC17xx\system_LPC17xx.h
+lpc17xx_pinsel.o: F:\Keil\ARM\Inc\NXP\LPC17xx\lpc_types.h
---
+++ NVIC/system_lpc17xx.d
@@ -0,0 +1,7 @@
+system_lpc17xx.o: ..\..\..\..\CMSIS\lpc175x_6x_cmsis_driver_library\Core\Device\NXP\LPC17xx\Source\Templates\system_LPC17xx.c
+system_lpc17xx.o: F:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+system_lpc17xx.o: F:\Keil\ARM\Inc\NXP\LPC17xx\LPC17xx.h
+system_lpc17xx.o: F:\Keil\ARM\CMSIS\Include\core_cm3.h
+system_lpc17xx.o: F:\Keil\ARM\CMSIS\Include\core_cmInstr.h
+system_lpc17xx.o: F:\Keil\ARM\CMSIS\Include\core_cmFunc.h
+system_lpc17xx.o: F:\Keil\ARM\Inc\NXP\LPC17xx\system_LPC17xx.h
